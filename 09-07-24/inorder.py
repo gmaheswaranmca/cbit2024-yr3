e2c1a78f93be9da832d6f903a4e3a8e4899f477f
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-07-09 11:41:40 +0530

    level order traversal - leetcode

---
+++ 09-07-24/inorder.py
@@ -0,0 +1,78 @@
+'''
+1. Inorder Traversal
+**Problem:** [Binary Tree Inorder Traversal]
+(https://leetcode.com/problems/binary-tree-inorder-traversal/)
+(https://www.hackerrank.com/challenges/tree-inorder-traversal/problem)
+
+def inOrder(root):
+    #base condition
+    if not root: # root != None
+        return
+    #Write your code here
+    inOrder(root.left)
+    print(root.info, end=' ')
+    inOrder(root.right)
+'''
+
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def inOrder(root):
+    #base condition
+    if not root: # root != None
+        return
+    #Write your code here
+    inOrder(root.left)
+    print(root.info, end=' ')
+    inOrder(root.right)
+
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+inOrder(tree.root)
